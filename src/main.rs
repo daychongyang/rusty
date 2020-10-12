761666073a0b4340c460bff9cbef889934c6887c
daychongyang <daychongyang@gmail.com>
2020-10-12 20:06:12 +0800

    feat: arr

--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,20 @@ fn main() {
     println!("binary: {}", binary);
     let byte = b'A';
     println!("byte: {}", byte);
+    // 字符类型
+    let c = 'z';
+    println!("c: {}", c);
+    // 元组类型
+    let tup = (500, 'A', true, 0.2);
+    let (decimal, charactor, truthy, floating_number) = tup;
+    println!("The value of decimal is: {}", decimal);
+    println!("The value of charactor is: {}", charactor);
+    println!("The value of truthy is: {}", truthy);
+    println!("The value of floating_number is: {}", floating_number);
+    println!("The value of floating_number is: {}", tup.3);
+
+    // 数组
+    let arr = [1, 2, 3];
+    let arr = [3; 5];
+    let arr: [i32; 5] = [1, 2, 3, 4, 5];
 }
