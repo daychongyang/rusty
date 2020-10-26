395bad687ac191d28511600956fe668024de501c
daychongyang <daychongyang@gmail.com>
2020-10-26 15:43:53 +0800

    feat: 结构体

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,48 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: i32,
+    height: i32,
+}
+
+impl Rectangle {
+    fn area(&self) -> i32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: i32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
-    let s = String::from("hello");
-    let ss = s.clone();
-    println!("{}", ss);
-    println!("{}", s)
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("The rectangle is {:#?}.", rect1);
+    println!("The square is {:#?}.", Rectangle::square(100));
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
