aeb7fbacdd3cafffae2f9ce593c759237eb91520
Day <daychongyang@gmail.com>
2021-04-25 22:41:12 +0800

    test: 表达式&语句

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 fn main() {
-    let tup: (i32, f64, u8) = (500, 6.4, 1);
-    let (x, y, z) = tup;
+    let x = 5;
+    let y = {
+        let x = 3;
+        x + 1
+    };
 
     println!("x is {}", x);
+
     println!("y is {}", y);
-    println!("z is {}", z);
 }
